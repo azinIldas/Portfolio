499f3f63c2cd1ada7815d30f5f3dd6b995d5eeb9
azin156 <azin4222@gmail.com>
2026-05-21 21:26:32 +0200

    feat: Update Portfolio

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -5,10 +5,4 @@ export default defineCliConfig({
     projectId: 'sj84f28g',
     dataset: 'production',
   },
-  /**
-   * Falls du das Studio später unter einem eigenen Subdomain
-   * deployen willst (z. B. https://portfolio-azin.sanity.studio):
-   *   npm run deploy
-   */
-  // studioHost: 'portfolio-azin',
 })
